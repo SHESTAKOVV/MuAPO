aba57b29883f4d34a8ef444ad62ae7fbdc09f965
SHESTAKOVV <hizayn666@gmail.com>
2025-06-01 05:10:36 +0700

    Добавление функции умножения

--- CalcApp/Calculator.cs
+++ CalcApp/Calculator.cs
@@ -10,7 +10,7 @@ namespace CalcApp
     {
         public static double Add(double a, double b) => 0;
         public static double Subtract(double a, double b) => 0;
-        public static double Multiply(double a, double b) => 0;
+        public static double Multiply(double a, double b) => a * b;
         public static double Divide(double a, double b) => 0;
     }
 }
