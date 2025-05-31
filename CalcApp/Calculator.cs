eda244aa5737412f5d16a7cf7bf1876b9215242c
SHESTAKOVV <hizayn666@gmail.com>
2025-06-01 04:45:50 +0700

    Базовая структура для последующего мержа с отдельными методами

---
+++ CalcApp/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcApp
+{
+    class Calculator
+    {
+        public static double Add(double a, double b) => 0;
+        public static double Subtract(double a, double b) => 0;
+        public static double Multiply(double a, double b) => 0;
+        public static double Divide(double a, double b) => 0;
+    }
+}
--- CalcApp/Program.cs
+++ CalcApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalcApp;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Операция (+ - * /):");
+        string op = Console.ReadLine();
+
+        double result = op switch
+        {
+            "+" => Calculator.Add(a, b),
+            "-" => Calculator.Subtract(a, b),
+            "*" => Calculator.Multiply(a, b),
+            "/" => Calculator.Divide(a, b),
+            _ => throw new Exception("Неизвестная операция")
+        };
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
