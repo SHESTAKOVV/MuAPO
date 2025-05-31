1159be7e786693410b064273d1559fd7a324154a
SHESTAKOVV <hizayn666@gmail.com>
2025-06-01 05:01:37 +0700

    Добавление функции сложения

--- CalcApp/Calculator.cs
+++ CalcApp/Calculator.cs
@@ -8,7 +8,7 @@ namespace CalcApp
 {
     class Calculator
     {
-        public static double Add(double a, double b) => 0;
+        public static double Add(double a, double b) => a + b;
         public static double Subtract(double a, double b) => 0;
         public static double Multiply(double a, double b) => 0;
         public static double Divide(double a, double b) => 0;
