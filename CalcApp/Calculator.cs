32faf701abd146c73b760d9e7df2750fb7894951
SHESTAKOVV <hizayn666@gmail.com>
2025-06-01 05:15:13 +0700

    Добавление функции деления

--- CalcApp/Calculator.cs
+++ CalcApp/Calculator.cs
@@ -11,6 +11,10 @@ namespace CalcApp
         public static double Add(double a, double b) => 0;
         public static double Subtract(double a, double b) => 0;
         public static double Multiply(double a, double b) => 0;
-        public static double Divide(double a, double b) => 0;
+        public static double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
     }
 }
